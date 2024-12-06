de1e128095f117fce10ab2597bfc2a8191fad8d0
bobowedge <mmbowen@gmail.com>
2024-12-06 01:39:22 -0500

    Day 6 updates

--- day06.py
+++ day06.py
@@ -1,83 +1,91 @@
+ROTATE_RIGHT = {(-1, 0) : (0, 1), (0, 1) : (1, 0), (1, 0) : (0, -1), (0, -1): (-1, 0)}
+
 def parse_data(data):
-    guard_pos = (None, None)
+    guard_init = (None, None)
     blocks = set()
     rows = len(data)
     cols = len(data[0])
     for row, line in enumerate(data):
-        
         for col, char in enumerate(line):
             if char == "^":
-                guard_pos = (row, col)
+                guard_init = (row, col)
             elif char == "#":
                 blocks.add((row, col))
-    return guard_pos, blocks, rows, cols
+    return guard_init, blocks, rows, cols
+
+def find_visited(initial_pos, initial_dir, blocks, rows, cols):
+    visited = set()
+    guard_position = initial_pos
+    guard_direction = initial_dir
+    while 0 <= guard_position[0] < rows and 0 <= guard_position[1] < cols:
+        visited.add(guard_position)
+        new_position = (guard_position[0] + guard_direction[0], guard_position[1] + guard_direction[1])
+        if new_position in blocks:
+            guard_direction = ROTATE_RIGHT[guard_direction]
+        else:
+            guard_position = new_position
+    return visited
 
 def solution1(data):
-    guard_pos, blocks, rows, cols = parse_data(data)
-    guard_dir = (-1, 0)
-    visited = set()
-    while 0 <= guard_pos[0] < rows and 0 <= guard_pos[1] < cols:
-        visited.add((guard_pos[0], guard_pos[1]))
-        new_guard_pos = (guard_pos[0]+guard_dir[0], guard_pos[1] + guard_dir[1])
-        if new_guard_pos in blocks:
-            if guard_dir == (-1, 0):
-                guard_dir = (0, 1)
-            elif guard_dir == (0, 1):
-                guard_dir = (1, 0)
-            elif guard_dir == (1, 0):
-                guard_dir = (0, -1)
-            else:
-                guard_dir = (-1, 0)
-        else:
-            guard_pos = new_guard_pos
+    init_pos, blocks, rows, cols = parse_data(data)
+    init_dir = (-1, 0)
+    visited = find_visited(init_pos, init_dir, blocks, rows, cols)
     return len(visited)
 
-
 def solution2(data):
-    guard_init, blocks, rows, cols = parse_data(data)
-    guard_pos = guard_init
-    guard_dir = (-1, 0)
-    visited = set()
-    while 0 <= guard_pos[0] < rows and 0 <= guard_pos[1] < cols:
-        visited.add(guard_pos)
-        new_guard_pos = (guard_pos[0]+guard_dir[0], guard_pos[1] + guard_dir[1])
-        if new_guard_pos in blocks:
-            if guard_dir == (-1, 0):
-                guard_dir = (0, 1)
-            elif guard_dir == (0, 1):
-                guard_dir = (1, 0)
-            elif guard_dir == (1, 0):
-                guard_dir = (0, -1)
-            else:
-                guard_dir = (-1, 0)
-        else:
-            guard_pos = new_guard_pos
+    # This is faster but uglier that the previous commit (14 seconds vs. 2 seconds)
+    init_pos, blocks, rows, cols = parse_data(data)
+    init_dir = (-1, 0)
+    visited = find_visited(init_pos, init_dir, blocks, rows, cols)
     
     loops = 0
     for v in visited:
-        blocks.add(v)
-        guard_pos = guard_init
-        guard_dir = (-1, 0)
-        guard_path = set()
-        guard_path.add((guard_pos, guard_dir))
-        while 0 <= guard_pos[0] < rows and 0 <= guard_pos[1] < cols:
-            new_guard_pos = (guard_pos[0]+guard_dir[0], guard_pos[1] + guard_dir[1])
-            if new_guard_pos in blocks:
-                if guard_dir == (-1, 0):
-                    guard_dir = (0, 1)
-                elif guard_dir == (0, 1):
-                    guard_dir = (1, 0)
-                elif guard_dir == (1, 0):
-                    guard_dir = (0, -1)
+        guard_position = init_pos
+        guard_direction = (-1, 0)
+        guard_path = {(guard_position, guard_direction)}
+        while 0 <= guard_position[0] < rows and 0 <= guard_position[1] < cols:
+            if guard_direction[0] == -1:
+                for row in range(guard_position[0] - 1, -1, -1):
+                    p = (row, guard_position[1])
+                    if p in blocks or p == v:
+                        guard_position = (p[0] + 1, guard_position[1])
+                        guard_direction = ROTATE_RIGHT[guard_direction]
+                        break
                 else:
-                    guard_dir = (-1, 0)
-            else:
-                guard_pos = new_guard_pos
-            if (guard_pos, guard_dir) in guard_path:
+                    guard_position = None
+            elif guard_direction[0] == 1:
+                for row in range(guard_position[0] + 1, rows + 1, 1):
+                    p = (row, guard_position[1])
+                    if p in blocks or p == v:
+                        guard_position = (p[0] - 1, guard_position[1])
+                        guard_direction = ROTATE_RIGHT[guard_direction]
+                        break
+                else:
+                    guard_position = None
+            elif guard_direction[1] == -1:
+                for col in range(guard_position[1] - 1, -1, -1):
+                    p = (guard_position[0], col)
+                    if p in blocks or p == v:
+                        guard_position = (guard_position[0], p[1] + 1)
+                        guard_direction = ROTATE_RIGHT[guard_direction]
+                        break
+                else:
+                    guard_position = None
+            elif guard_direction[1] == 1:
+                for col in range(guard_position[1] + 1, cols + 1, 1):
+                    p = (guard_position[0], col)
+                    if p in blocks or p == v:
+                        guard_position = (guard_position[0], p[1] - 1)
+                        guard_direction = ROTATE_RIGHT[guard_direction]
+                        break
+                else:
+                    guard_position = None
+            if guard_position is None:
+                break
+            if (guard_position, guard_direction) in guard_path:
                 loops += 1
                 break
-            guard_path.add((guard_pos, guard_dir))
-        blocks.remove(v)
+            guard_path.add((guard_position, guard_direction))
     return loops
     
     
@@ -95,4 +103,10 @@ assert(solution1(test_data) == 41)
 assert(solution2(test_data) == 6)
 my_data = open("data/day06.txt").read().splitlines()
 print(f"Solution 1: {solution1(my_data)}")
+# print(f"Solution 2: {solution2(my_data)}")
+
+import time
+t1 = time.process_time()
 print(f"Solution 2: {solution2(my_data)}")
+t2 = time.process_time()
+print(t2 - t1)
